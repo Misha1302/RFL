95738301b5fb8d775b319b445845738f66fe7118
Михаил <misha13022008@gmail.com>
2024-05-15 12:00:16 +0300

    jumping refactored

--- Assets/RFL/Scripts/GameLogic/Player/PlayerJumper.cs
+++ Assets/RFL/Scripts/GameLogic/Player/PlayerJumper.cs
@@ -9,16 +9,14 @@
     public class PlayerJumper : MonoBeh
     {
         [SerializeField] private AnimationCurve jumpVelocityCurve;
-        [SerializeField] private float jumpTime = 1;
-        [SerializeField] private float jumpHeight = 10;
+        [SerializeField] private float jumpTime = 0.75f;
+        [SerializeField] private float jumpHeight = 8f;
 
         private bool _jumped;
         private float _startJumpTime = float.MinValue;
 
 
-        private bool IsJumping =>
-            _startJumpTime + jumpTime >= Services.TimeService.Time && Services.InputService.Jump &&
-            !GroundChecker.IsGrounded;
+        private bool IsJumping => jumpTime >= PassedTime && Services.InputService.Jump && !GroundChecker.IsGrounded;
 
         private float PassedTime => Services.TimeService.Time - _startJumpTime;
 
@@ -39,15 +37,37 @@
 
         private void HandleJumpIfNeed()
         {
-            if (_startJumpTime + jumpTime < Services.TimeService.Time && Services.InputService.Jump &&
-                GroundChecker.IsGrounded)
-                Jump();
+            if (jumpTime >= PassedTime || !Services.InputService.Jump || !GroundChecker.IsGrounded) return;
+            HandleJump();
         }
 
         private void HandleEndOfJumpingIfNeed()
         {
-            if (IsJumping || !_jumped) return;
+            if (PassedTime < 0.1f || IsJumping || !_jumped) return;
+            HandleEndOfJumping();
+        }
 
+        private void HandleJumpingIfNeed()
+        {
+            if (!IsJumping) return;
+            HandleJumping();
+        }
+
+        private void HandleJumping()
+        {
+            Rb.AddForce(Vector2.up * GetYForce());
+        }
+
+        private void HandleJump()
+        {
+            _startJumpTime = Services.TimeService.Time;
+            _jumped = true;
+
+            Rb.velocity = Rb.velocity.WithY(GetYForce());
+        }
+
+        private void HandleEndOfJumping()
+        {
             _jumped = false;
             _startJumpTime = float.MinValue;
 
@@ -56,25 +76,13 @@
 
         private void SlowDownIfNeed()
         {
-            if (Rb.velocity.y > 0)
-                Rb.velocity = Rb.velocity.WithY(Rb.velocity.y / 2f);
+            if (Rb.velocity.y <= 0) return;
+            SlowDown();
         }
 
-        private void HandleJumpingIfNeed()
+        private void SlowDown()
         {
-            if (!IsJumping) return;
-
-            _jumped = true;
-
-            // set velocity when grounded to remove error with fast acceleration
-            if (GroundChecker.IsGrounded) Rb.velocity = Rb.velocity.WithY(GetYForce());
-            else Rb.AddForce(Vector2.up * GetYForce());
-        }
-
-        private void Jump()
-        {
-            _startJumpTime = Services.TimeService.Time;
-            Rb.velocity = Rb.velocity.WithY(GetYForce());
+            Rb.velocity = Rb.velocity.WithY(Rb.velocity.y / 2f);
         }
 
         private float GetYForce() => jumpVelocityCurve.Evaluate(PassedTime / jumpTime) * jumpHeight;
