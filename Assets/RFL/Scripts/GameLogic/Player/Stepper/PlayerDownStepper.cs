b1ce7136b5bf1255e385278e8f7eae8bab3e9520
Михаил <misha13022008@gmail.com>
2024-05-23 13:22:50 +0300

    down stepper fixed

--- Assets/RFL/Scripts/GameLogic/Player/Stepper/PlayerDownStepper.cs
+++ Assets/RFL/Scripts/GameLogic/Player/Stepper/PlayerDownStepper.cs
@@ -32,23 +32,40 @@
             var castLeft = Raycast(_playerStepper.LeftRayPoint.position);
             var castRight = Raycast(_playerStepper.RightRayPoint.position);
 
-            if (castLeft.WasHit && !castRight.WasAnyHitOnPath) Player.PlayerTransform.MoveToY(castLeft.HitPoint.y);
-            if (castRight.WasHit && !castLeft.WasAnyHitOnPath) Player.PlayerTransform.MoveToY(castRight.HitPoint.y);
-            if (castRight.WasHit && castLeft.WasHit)
-                Player.PlayerTransform.MoveToY(Mathf.Max(castLeft.HitPoint.y, castRight.HitPoint.y) + (Player.PlayerStepper.Player2FootsDelta));
+            float y = 0;
+            if (castLeft.WasHit && !castRight.WasAnyHitOnPath)
+                y = castLeft.HitPoint.y;
+            else if (!castLeft.WasAnyHitOnPath && castRight.WasHit)
+                y = castRight.HitPoint.y;
+            else if (castRight.WasHit && castLeft.WasHit)
+                y = Mathf.Max(castLeft.HitPoint.y, castRight.HitPoint.y);
+
+            Player.PlayerTransform.MoveToY(CalcY(y));
         }
+
+        private static float CalcY(float y) => y + Player.PlayerStepper.Player2FootsDelta;
 
         private StepperRaycastInfo Raycast(Vector3 point)
         {
             var count = Physics2D.Raycast(point, Vector2.down, _contactFilter2D, _hits, _playerStepper.MaxStep);
-            var hit = _hits.Slice(0, count)
+            var hits = _hits.Slice(0, count)
+                .Where(x => !x.transform.HasComponent<PlayerTag>())
                 .OrderByDescending(x => x.point.y)
-                .FirstOrDefault(x => point.y - x.point.y > _playerStepper.MinStep && !x.transform.HasComponent<PlayerTag>());
+                .ToArray();
 
+            var hit = hits.FirstOrDefault();
+            if (point.y - hit.point.y < _playerStepper.MinStep)
+                hit = default;
+
+            DrawDownStepper(point, hit);
+
+            return new StepperRaycastInfo(hit ? hit.point : Vector2.zero.MakeNan(), hits.Length != 0);
+        }
+
+        private void DrawDownStepper(Vector3 point, RaycastHit2D hit)
+        {
             Debug.DrawLine(point, point + Vector3.down * _playerStepper.MaxStep, Color.red);
             Debug.DrawLine(point, hit.point, Color.green);
-
-            return new StepperRaycastInfo(hit ? hit.point : Vector2.zero.MakeNan(), count != 0);
         }
     }
 }
