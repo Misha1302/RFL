449bd47345f23fb687c01055e4692ed0cfb75e84
Михаил <misha13022008@gmail.com>
2024-05-23 11:26:46 +0300

    time setter added

---
+++ Assets/RFL/Scripts/EditorHelpers/TimeSetter.cs
@@ -0,0 +1,14 @@
+﻿namespace RFL.Scripts.EditorHelpers
+{
+    using UnityEngine;
+
+    public class TimeSetter : MonoBehaviour
+    {
+        [Range(0.1f, 2f)] [SerializeField] private float scale;
+
+        private void OnValidate()
+        {
+            Time.timeScale = scale;
+        }
+    }
+}
